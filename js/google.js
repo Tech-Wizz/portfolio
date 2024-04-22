ee5537460c1d7d06113ef0f52decb08b97a2c4a4
Kruize Derek Christensen <christensen.kruize@gmail.com>
2024-04-22 07:55:22 -0600

    Create google.js

---
+++ js/google.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4S0XW0JHHG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4S0XW0JHHG');
+</script>
