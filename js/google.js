601a22bcf76bcf3fd079f32ba4ff730ac9e9095c
Kruize Derek Christensen <christensen.kruize@gmail.com>
2024-04-22 08:01:37 -0600

    Update google.js
    fixed to be an actual js file

--- js/google.js
+++ js/google.js
@@ -1,9 +1,5 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-4S0XW0JHHG"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];  
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-4S0XW0JHHG');
-</script>
+gtag('config', 'G-4S0XW0JHHG');
